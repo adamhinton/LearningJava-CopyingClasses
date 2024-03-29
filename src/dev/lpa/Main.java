0cc6280c59a005bff8c846f39340bce686af31cf
adamd <adam.d.hinton@gmail.com>
2024-03-29 09:37:30 -0400

    Equal ref check

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,5 +1,7 @@
 package dev.lpa;
 
+import java.util.Arrays;
+
 record Person(String name, String dob){}
 
 public class Main {
@@ -10,6 +12,17 @@ public class Main {
         Person jack = new Person ("Jack", "03/03/1963");
         Person jane = new Person ("Jane", "04/04/1964");
         Person jill = new Person ("Jill", "05/05/1965");
+
+        Person[] persons = {joe, jim, jack, jane, jill};
+        // shallow copy
+        // deep copy not needed bc there's no way to mutate Person records
+        Person[] personsCopy = Arrays.copyOf(persons, persons.length);
+
+        for (int i=0; i<5; i++){
+            if(persons[i] == personsCopy[i]){
+                System.out.println("Equal References " + persons[i]);
+            }
+        }
 
     }
 }
